94fe0d463779b2fc308f405d5eb9d82db6eaea56
Jonathan Templin <jonathan-templin@uiowa.edud>
2022-10-04 08:06:13 -0500

    last example with location/scale model

--- lectures/lecture03c/03c_Bayesian_Model_Fit_and_Comparisons.R
+++ lectures/lecture03c/03c_Bayesian_Model_Fit_and_Comparisons.R
@@ -184,4 +184,125 @@ model06b_Samples$loo()
 # comparing two models with loo:
 loo_compare(list(model06_Samples$loo(), model06b_Samples$loo()))
 
+# final comparison: investigating homogeneity of variance assumption
+
+model07_Syntax = "
+
+data {
+  int<lower=0> N;         // number of observations
+  int<lower=0> P;         // number of predictors (plus column for intercept)
+  matrix[N, P] X;         // model.matrix() from R 
+  vector[N] y;            // outcome
+  
+  vector[P] meanBeta;       // prior mean vector for coefficients
+  matrix[P, P] covBeta; // prior covariance matrix for coefficients
+  
+  vector[P] meanGamma;       // prior mean vector for coefficients
+  matrix[P, P] covGamma; // prior covariance matrix for coefficients
+}
+
+
+parameters {
+  vector[P] beta;         // vector of coefficients for conditional mean
+  vector[P] gamma;        // vector of coefficients for residual sd
+}
+
+
+model {
+  beta ~ multi_normal(meanBeta, covBeta); // prior for coefficients
+  gamma ~ multi_normal(meanGamma, covGamma); // prior for coefficients
+  y ~ normal(X*beta, exp(X*gamma));              // linear model
+}
+
+generated quantities{
+
+  // general quantities used below:
+  vector[N] y_pred;
+  vector[N] y_sd;
+  y_pred = X*beta; // predicted value (conditional mean)
+  y_sd = exp(X*gamma); // conditional standard deviation
+  
+  // posterior predictive model checking
+  
+  array[N] real y_rep;
+  y_rep = normal_rng(y_pred, y_sd);
+  
+  real mean_y = mean(y);
+  real sd_y = sd(y);
+  real mean_y_rep = mean(to_vector(y_rep));
+  real<lower=0> sd_y_rep = sd(to_vector(y_rep));
+  int<lower=0, upper=1> mean_gte = (mean_y_rep >= mean_y);
+  int<lower=0, upper=1> sd_gte = (sd_y_rep >= sd(y));
+  
+  // WAIC and LOO for model comparison
+  
+  array[N] real log_lik;
+  for (person in 1:N){
+    log_lik[person] = normal_lpdf(y[person] | y_pred[person], y_sd[person]);
+  }
+}
+
+"
+
+# compile stan code into executable
+model07_Stan = cmdstan_model(stan_file = write_stan_file(model07_Syntax))
+
+
+# start with model formula
+model07_formula = formula(WeightLB ~ Height60IN + factor(DietGroup) + Height60IN:factor(DietGroup), data = DietData)
+
+# grab model matrix
+model07_predictorMatrix = model.matrix(model07_formula, data=DietData)
+
+# find details of model matrix
+N = nrow(model07_predictorMatrix)
+P = ncol(model07_predictorMatrix)
+
+# build matrices of hyper parameters (for priors)
+meanBeta = rep(0, P)
+covBeta = diag(x = 1000, nrow = P, ncol = P)
+meanGamma = rep(0, P)
+covGamma = diag(x = 1000, nrow = P, ncol = P)
+
+
+
+# build Stan data from model matrix
+model07_data = list(
+  N = N,
+  P = P,
+  X = model06_predictorMatrix,
+  y = DietData$WeightLB,
+  meanBeta = meanBeta,
+  covBeta = covBeta,
+  meanGamma = meanGamma,
+  covGamma = covGamma
+)
+
+
+model07_Samples = model07_Stan$sample(
+  data = model07_data,
+  seed = 04102022,
+  chains = 4,
+  parallel_chains = 4,
+  iter_warmup = 2000,
+  iter_sampling = 4000
+)
+
+# maximum R-hat
+max(model07_Samples$summary()$rhat, na.rm = TRUE)
+
+# model results
+View(model07_Samples$summary())
+model07_Samples$summary(variables = "gamma")
+
+# model comparisons
+waic(x = model06_Samples$draws("log_lik"))
+waic(x = model07_Samples$draws("log_lik"))
+
+model06_Samples$loo()
+model07_Samples$loo()
+
+loo_compare(list(homogeneous = model06_Samples$loo(), heterogeneous = model07_Samples$loo()))
+
+
 save.image(file = "lecture03c.RData")
