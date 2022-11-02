339c24d3f2d59bf79f968acfe81512616a65a383
Jonathan Templin <jonathan-templin@uiowa.edud>
2022-11-02 10:29:31 -0500

    new R script for lecture 04b

--- lectures/lecture04b/04b_Modeling_Observed_Data.R
+++ lectures/lecture04b/04b_Modeling_Observed_Data.R
@@ -106,9 +106,11 @@ max(modelCFA_samples$summary()$rhat, na.rm = TRUE)
 # item parameter results
 print(modelCFA_samples$summary(variables = c("mu", "lambda", "psi")) ,n=Inf)
 
+# showing relationship between item means and mu parameters
+apply(X = conspiracyItems, MARGIN = 2, FUN = mean)
 
 # investigating item parameters ================================================
-itemNumber = 3
+itemNumber = 10
 
 labelMu = paste0("mu[", itemNumber, "]")
 labelLambda = paste0("lambda[", itemNumber, "]")
@@ -137,9 +139,13 @@ y = itemSummary$mean[which(itemSummary$variable==labelMu)] +
   itemSummary$mean[which(itemSummary$variable==labelLambda)]*theta
 lines(x = theta, y = y, lwd = 5, lty=3, col=2)
 
+mcmc_dens(modelCFA_samples$draws(variables = "lambda[10]"))
+
 # legend
 legend(x = -3, y = 7, legend = c("Posterior Draw", "Item Limits", "EAP"), col = c(1,4,2), lty = c(1,2,3), lwd=5)
 
+plot(x=itemParameters[,1], y=itemParameters[,2])
+cor(x=itemParameters[,1], y=itemParameters[,2])
 
 # investigating latent variables
 
@@ -177,6 +183,79 @@ plot(y = modelCFA_samples$summary(variables = c("theta"))$sd, x = modelCFA_sampl
 ## comparing EAP estimates with sum scores
 plot(x = rowSums(conspiracyItems), y = modelCFA_samples$summary(variables = c("theta"))$mean,
      xlab = "Sum Score", ylab = expression(theta))
+
+# Estimating Theta with fixed item parameters ==================================
+lambdaEst = modelCFA_samples$summary(variables = "lambda")$mean
+muEst = modelCFA_samples$summary(variables = "mu")$mean
+psiEst = modelCFA_samples$summary(variables = "psi")$mean
+
+
+modelCFAfixedItems_syntax = "
+
+data {
+  int<lower=0> nObs;                 // number of observations
+  int<lower=0> nItems;               // number of items
+  matrix[nObs, nItems] Y;            // item responses in a matrix
+
+  vector[nItems] muEst;
+  vector[nItems] lambdaEst;
+  vector[nItems] psiEst;
+}
+
+parameters {
+  vector[nObs] theta;                // the latent variables (one for each person)
+}
+
+model {
+  
+  theta ~ normal(0, 1);                         // Prior for latent variable (with mean/sd specified)
+  
+  for (item in 1:nItems){
+    Y[,item] ~ normal(muEst[item] + lambdaEst[item]*theta, psiEst[item]);
+  }
+  
+}
+
+"
+
+modelCFAfixedItems_stan = cmdstan_model(stan_file = write_stan_file(modelCFAfixedItems_syntax))
+
+modelCFAfixedItems_data = list(
+  nObs = nObs,
+  nItems = nItems,
+  Y = conspiracyItems, 
+  muEst = muEst,
+  lambdaEst = lambdaEst,
+  psiEst = psiEst
+)
+
+modelCFAfixedItems_samples = modelCFAfixedItems_stan$sample(
+  data = modelCFAfixedItems_data,
+  seed = 28102022,
+  chains = 4,
+  parallel_chains = 4,
+  iter_warmup = 5000,
+  iter_sampling = 50000
+)
+
+fixedItems_ThetaMeans = modelCFAfixedItems_samples$summary(variables = c("theta"))$mean
+fixedItems_ThetaSDs = modelCFAfixedItems_samples$summary(variables = c("theta"))$sd
+
+plot(y = fixedItems_ThetaSDs, 
+     x = fixedItems_ThetaMeans,
+     xlab = "E(theta|Y)", ylab = "SD(theta|Y)")
+
+estimatedItems_ThetaMeans = modelCFA_samples$summary(variables = c("theta"))$mean
+estimatedItems_ThetaSDs = modelCFA_samples$summary(variables = c("theta"))$sd
+
+plot(y = estimatedItems_ThetaMeans, 
+     x = fixedItems_ThetaMeans,
+     xlab = "Fixed Item Parameters", ylab = "Estimated Item Parameters", main = "EAP Theta Estimates")
+
+plot(y = estimatedItems_ThetaSDs, 
+     x = fixedItems_ThetaSDs,
+     xlab = "Fixed Item Parameters", ylab = "Estimated Item Parameters", main = "Theta Posterior SDs")
+
 
 # Convergence Fails ============================================================
 
